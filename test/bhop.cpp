7c7cf56370c2ec1891cb4f6b54e7b0db764abec5
Владислав Алпатов <alpatof74@gmail.com>
2021-02-27 22:10:21 +0300

    обновление bhoр'а

--- test/bhop.cpp
+++ test/bhop.cpp
@@ -1,16 +1,26 @@
 #include "bhop.h"
 
+class LocalPlayer
+{
+public:
+    char pad_0000[244]; //0x0000
+    int m_Team; //0x00F4
+    char pad_00F6[14]; //0x00F6
+    int m_Flags; //0x0104
+    char pad_0108[3896]; //0x0108
+}; //Size: 0x1040
+
 void HandleBhop(DWORD baseAddr)
 {
     __try
     {
-        DWORD LocalPlayer = *(DWORD*)(baseAddr + signatures::dwLocalPlayer);
+        DWORD LocalPlayer = *mem::ReadVirtualMemory<DWORD>(baseAddr + signatures::dwLocalPlayer);
 
-        if (*(int*)(LocalPlayer + netvars::m_fFlags) > 256 and GetAsyncKeyState(VK_SPACE))
+        if (*mem::ReadVirtualMemory<DWORD>(LocalPlayer + netvars::m_fFlags) > 256 and GetAsyncKeyState(VK_SPACE))
         {
-            *(int*)(baseAddr + signatures::dwForceJump) = 5;
+            mem::WriteVirtualMemory<int>(baseAddr + signatures::dwForceJump, 5);
             Sleep(10);
-            *(int*)(baseAddr + signatures::dwForceJump) = 4;
+            mem::WriteVirtualMemory<int>(baseAddr + signatures::dwForceJump, 4);
         }
 
     }
