678bebe0a2aa9041ed21daef6dee9141c12a81ac
Владислав Алпатов <alpatof74@gmail.com>
2021-03-03 19:53:59 +0300

    обновление смещений

--- test/CBaseEntity.h
+++ test/CBaseEntity.h
@@ -5,10 +5,17 @@
 class CBaseEntity
 {
 public:
+
+	Vector3& get_origin()
+	{
+		return *reinterpret_cast<Vector3*>(reinterpret_cast<uintptr_t>(this) + 0x138);
+	}
+
 	union 
 	{
 		DEFINE_MEMBER_N(int, m_iTeamNum, netvars::m_iTeamNum);
 		DEFINE_MEMBER_N(int, m_iHealth, netvars::m_iHealth);
+		DEFINE_MEMBER_N(int, m_iTeamNum, netvars::m_iTeamNum);
 		DEFINE_MEMBER_N(int, m_fFlags, netvars::m_fFlags);
 		DEFINE_MEMBER_N(Vec3, m_vecOrigin, 0x138);
 		DEFINE_MEMBER_N(int, m_iDefaultFOV, netvars::m_iDefaultFOV);
