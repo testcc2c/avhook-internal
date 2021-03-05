9489f55ce88801c381825d423cdc53b938d9cf75
Владислав Алпатов <alpatof74@gmail.com>
2021-03-05 21:29:25 +0300

    фикс

--- test/CBaseEntity.h
+++ test/CBaseEntity.h
@@ -10,21 +10,11 @@ public:
 		DEFINE_MEMBER_N(int, m_iTeamNum, netvars::m_iTeamNum);
 		DEFINE_MEMBER_N(int, m_iHealth, netvars::m_iHealth);
 		DEFINE_MEMBER_N(int, m_fFlags, netvars::m_fFlags);
+		DEFINE_MEMBER_N(Vec3, m_vecOrigin, 0x138);
 		DEFINE_MEMBER_N(int, m_iDefaultFOV, netvars::m_iDefaultFOV);
 		DEFINE_MEMBER_N(int, m_iGlowIndex, netvars::m_iGlowIndex);
 		DEFINE_MEMBER_N(int, m_ArmorValue, netvars::m_ArmorValue);
 		DEFINE_MEMBER_N(int, m_iCrosshairId, netvars::m_iCrosshairId);
-
 	};
-	Vector3 getOrigin()
-	{
-		DWORD player = *(DWORD*)((DWORD)GetModuleHandle(L"client.dll") + signatures::dwLocalPlayer);
-
-		Vector3 pos = *(Vector3*)(player + 0x138);
-
-		return pos;
-	}
 private:
-	DWORD baseAddr;
-
 };
