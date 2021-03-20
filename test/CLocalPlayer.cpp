6439864b80b9f9aa7d0783b5ce97d280d1e8fb87
Владислав Алпатов <alpatof74@gmail.com>
2021-03-20 14:13:32 +0300

    фикм расчётов в CLocalPlayer::AimAt

--- test/CLocalPlayer.cpp
+++ test/CLocalPlayer.cpp
@@ -8,8 +8,10 @@ void CLocalPlayer::AimAt(CBaseEntity*& entity, int bone, int time, bool predicti
 
 	ImVec3 targetpos = entity->GetBonePosition(bone);
 	ImVec3 myPos = this->m_vecOrigin;
+	myPos.y += this->m_vecViewOffset.y;
+	myPos.x += this->m_vecViewOffset.x;
 	myPos.z += this->m_vecViewOffset.z;
-
+	
 
 
 	ImVec3* angles = (ImVec3*)(*(DWORD*)(engineModule + signatures::dwClientState) + signatures::dwClientState_ViewAngles);
@@ -23,7 +25,7 @@ void CLocalPlayer::AimAt(CBaseEntity*& entity, int bone, int time, bool predicti
 		distance = sqrtf(powf(targetpos.x - myPos.x, 2) + powf(targetpos.y - myPos.y, 2) + powf(targetpos.z - myPos.z, 2));
 	}
 
-	float pitch = -asinf( (targetpos.z - myPos.z) / distance * (180 / 3.1415926f));
+	float pitch = -asinf( (targetpos.z - myPos.z) / distance) * (180 / 3.1415926f);
 	float yaw = atan2f(targetpos.y - myPos.y, targetpos.x - myPos.x) * (180 / 3.1415926f);
 
 	if ((-89.f <= pitch) && (pitch <= 89.f) && (-180.f <= yaw) && (yaw <= 180.f))
