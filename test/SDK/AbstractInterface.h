deac772f9b9d6d197338ed571797d9ecf7d5ca9b
Владислав Алпатов <alpatof74@gmail.com>
2021-07-22 18:25:31 +0300

    Refactored architecture

---
+++ test/SDK/AbstractInterface.h
@@ -0,0 +1,13 @@
+#pragma once
+class AbstractInterface
+{
+protected:
+	template<typename FuncType>
+	FuncType GetVirtualFunction(unsigned int index)
+	{
+		unsigned int* pVTable         = *reinterpret_cast<unsigned int**>(this);
+		unsigned int function_address = pVTable[index];
+		return reinterpret_cast<FuncType>(function_address);
+	}
+};
+
